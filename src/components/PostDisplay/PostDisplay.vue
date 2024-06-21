0d8b1082d53d1a6a141a305bb7591f59c8e87c2b
Romulo Iorio <romuloiorio@hotmail.com>
2024-06-21 10:50:11 -0300

    feat: implementing post update

--- src/components/PostDisplay/PostDisplay.vue
+++ src/components/PostDisplay/PostDisplay.vue
@@ -1,12 +1,24 @@
 <script setup lang="ts">
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faTrash, faPencil, faCheck, faX } from '@fortawesome/free-solid-svg-icons'
 
 import type { Props } from './usePostDisplay'
 import { usePostDisplay } from './usePostDisplay'
 
 const props = defineProps<Props>()
-const { formatDate, hovered, setHovered, onDelete } = usePostDisplay(props)
+const {
+  formatDate,
+  hovered,
+  setHovered,
+  onDelete,
+  isEditing,
+  setIsEditing,
+  onEdit,
+  newContent,
+  newTitle,
+  onChangeContent,
+  onChangeTitle
+} = usePostDisplay(props)
 const { post } = props
 </script>
 
@@ -17,24 +29,63 @@ const { post } = props
     @mouseover="setHovered(true)"
     :key="post.id"
   >
-    <h1 class="text-white text-2xl font-bold">{{ post.title }}</h1>
+    <h1 class="text-white text-2xl font-bold w-fit p-2" v-if="!isEditing">{{ post.title }}</h1>
 
-    <p class="text-white break-words whitespace-pre-line text-lg font-light">
+    <input
+      class="text-white text-2xl font-bold w-fit rounded-lg p-2 bg-black/20"
+      v-if="isEditing"
+      v-model="newTitle"
+      @input="onChangeTitle"
+    />
+
+    <p class="text-white break-words whitespace-pre-line text-lg font-light p-2" v-if="!isEditing">
       {{ post.content }}
     </p>
+
+    <textarea
+      class="text-white bg-transparent break-words whitespace-pre-line text-lg font-light p-2 bg-black/20"
+      v-if="isEditing"
+      v-model="newContent"
+      @input="onChangeContent"
+    />
 
     <span>
       <span class="text-gray-400 text-sm">Postado por {{ post.user_id }} em </span>
       <span class="text-gray-400 text-sm">{{ formatDate(post.created_at) }}</span>
     </span>
 
-    <button
-      class="bg-red-500 text-white p-2 rounded-lg w-10 h-10 hover:bg-red-600 active:bg-red-700 transition duration-200 absolute top-4 right-4"
-      @click="onDelete"
-      v-if="hovered"
-    >
-      <FontAwesomeIcon :icon="faTrash" />
-    </button>
+    <div class="absolute top-4 right-4 flex gap-4" v-if="hovered">
+      <button
+        class="bg-green-500 text-white p-2 rounded-lg w-10 h-10 hover:bg-green-600 active:bg-green-700 transition duration-200"
+        @click="onEdit"
+        v-if="isEditing"
+      >
+        <FontAwesomeIcon :icon="faCheck" />
+      </button>
+
+      <button
+        class="bg-red-500 text-white p-2 rounded-lg w-10 h-10 hover:bg-red-600 active:bg-red-700 transition duration-200"
+        @click="setIsEditing(false)"
+        v-if="isEditing"
+      >
+        <FontAwesomeIcon :icon="faX" />
+      </button>
+
+      <button
+        class="bg-gray-500 text-white p-2 rounded-lg w-10 h-10 hover:bg-gray-600 active:bg-gray-700 transition duration-200"
+        @click="setIsEditing(true)"
+        v-if="!isEditing"
+      >
+        <FontAwesomeIcon :icon="faPencil" />
+      </button>
+
+      <button
+        class="bg-red-500 text-white p-2 rounded-lg w-10 h-10 hover:bg-red-600 active:bg-red-700 transition duration-200"
+        @click="onDelete"
+      >
+        <FontAwesomeIcon :icon="faTrash" />
+      </button>
+    </div>
   </div>
 </template>
 
--- src/components/PostDisplay/usePostDisplay.ts
+++ src/components/PostDisplay/usePostDisplay.ts
@@ -1,3 +1,5 @@
+import { toast } from 'vue3-toastify'
+
 import type { Post } from '@/interfaces'
 import type { Dispatch } from '@/states'
 
@@ -12,12 +14,60 @@ export interface Props {
 export const usePostDisplay = ({ setPosts, post }: Props) => {
   const formatDate = (date: string) => new Date(date).toLocaleString('pt-BR')
 
+  const [isEditing, setIsEditing] = useState(false)
   const [hovered, setHovered] = useState(false)
+  const [newContent, setNewContent] = useState(post.content)
+  const [newTitle, setNewTitle] = useState(post.title)
 
   const onDelete = async () => {
-    await api.posts.delete(post.id)
-    setPosts((prev) => prev.filter((p) => p.id !== post.id))
+    try {
+      await api.posts.delete(post.id)
+      setPosts((prev) => prev.filter((p) => p.id !== post.id))
+    } catch (e) {
+      console.error(e)
+      toast.error('Erro ao deletar post...')
+    }
   }
 
-  return { formatDate, hovered, setHovered, onDelete }
+  const onEdit = async () => {
+    const newPost = { ...post, content: newContent.value, title: newTitle.value }
+
+    try {
+      const updatedPost = await api.posts.update(newPost)
+      setPosts((prev) =>
+        prev.map((p) => {
+          const isUpdatedPost = p.id === updatedPost.id
+          if (isUpdatedPost) return updatedPost
+          return p
+        })
+      )
+    } catch (e) {
+      console.error(e)
+      toast.error('Erro ao editar post...')
+    }
+  }
+
+  const onChangeTitle = (event: Event) => {
+    const target = event.target as HTMLInputElement
+    setNewTitle(target.value)
+  }
+
+  const onChangeContent = (event: Event) => {
+    const target = event.target as HTMLTextAreaElement
+    setNewContent(target.value)
+  }
+
+  return {
+    formatDate,
+    hovered,
+    setHovered,
+    onDelete,
+    isEditing,
+    setIsEditing,
+    onEdit,
+    newContent,
+    newTitle,
+    onChangeTitle,
+    onChangeContent
+  }
 }
